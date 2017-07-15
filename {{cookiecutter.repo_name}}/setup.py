ec7a17931dbdc16616fce00efcc0982883e68829
David Gidwani <david.gidwani@gmail.com>
2017-07-15 19:42:25 -0400

    updated requireemnts, cleaned up tox.ini, and removed broken kwarg in setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -69,7 +69,6 @@ def setup():
             'Topic :: Utilities',
         ],
         description=__doc__,
-        discover_requirements=True,
         entry_points={
             'console_scripts': [
                 '{{cookiecutter.package_name}} = {{cookiecutter.package_name}}.cli:main',
